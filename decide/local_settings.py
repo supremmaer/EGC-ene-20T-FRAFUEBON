5324f54fc047756458624838100541d96e07f712
Javier de la Fuente <xavy14op@gmail.com>
2020-01-08 19:22:17 +0100

    local_settings ready for docker deploy

--- decide/local_settings.py
+++ decide/local_settings.py
@@ -1,4 +1,18 @@
-ALLOWED_HOSTS = ["*"]
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+STATIC_ROOT = '/app/static/'
+MEDIA_ROOT = '/app/static/media/'
+ALLOWED_HOSTS = ['*']
 
 # Modules in use, commented modules that you won't use
 MODULES = [
@@ -13,32 +27,17 @@ MODULES = [
     'voting',
 ]
 
-BASEURL =  'http://localhost:8000'
+BASEURL = 'http://10.5.0.1:8000'
+
 
 APIS = {
-    'authentication': 'http://localhost:8000',
-    'base': 'http://localhost:8000',
-    'booth': 'http://localhost:8000',
-    'census': 'http://localhost:8000',
-    'mixnet': 'http://localhost:8000',
-    'postproc': 'http://localhost:8000',
-    'store': 'http://localhost:8000',
-    'visualizer': 'http://localhost:8000',
-    'voting': 'http://localhost:8000',
+    'authentication': BASEURL,
+    'base': BASEURL,
+    'booth': BASEURL,
+    'census': BASEURL,
+    'mixnet': BASEURL,
+    'postproc': BASEURL,
+    'store': BASEURL,
+    'visualizer': BASEURL,
+    'voting': BASEURL,
 }
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'decide',
-        'USER': 'decide',
-        'PASSWORD' : 'decide',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
-# number of bits for the key, all auths should use the same number of bits
-
-KEYBITS = 128
